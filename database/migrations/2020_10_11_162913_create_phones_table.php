ba37731975ad278234f4d9b9ef98de2d7617b4c4
Farid Hidayat <e.faridhidayat@gmail.com>
2022-01-21 00:53:24 +0700

    feat: phones migration file

---
+++ database/migrations/2020_10_11_162913_create_phones_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contact_phones', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('contact_id')->constrained('contacts');
+            $table->enum('type', ['mobile', 'home', 'work', 'school', 'iPhone', 'Android', 'main', 'home fax', 'work fax', 'pager', 'other'])->default('mobile');
+            $table->string('phone');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
