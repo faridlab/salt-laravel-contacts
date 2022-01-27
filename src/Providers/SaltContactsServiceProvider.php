0feb5c7a57fbfef50fbaeaa593158229798bdba6
Farid Hidayat <e.faridhidayat@gmail.com>
2022-01-27 20:13:33 +0700

    feat: ContactsProvider

--- src/Providers/SaltContactsServiceProvider.php
+++ src/Providers/SaltContactsServiceProvider.php
@@ -18,9 +18,9 @@ class SaltContactsServiceProvider extends ServiceProvider
          *
          * Uncomment this function call to make the config file publishable using the 'config' tag.
          */
-        // $this->publishes([
-        //     __DIR__.'/../../config/salt-laravel-contacts.php' => config_path('salt-laravel-contacts.php'),
-        // ], 'config');
+        $this->publishes([
+            __DIR__.'/../../config/salt-contacts.php' => config_path('salt-contacts.php'),
+        ], 'contacts-config');
 
         /**
          * Routes
@@ -29,6 +29,7 @@ class SaltContactsServiceProvider extends ServiceProvider
          * A web.php file has already been generated.
          */
         // $this->loadRoutesFrom(__DIR__.'/../../routes/web.php');
+        $this->loadRoutesFrom(__DIR__.'/../../routes/api.php');
 
         /**
          * Translations
@@ -37,10 +38,10 @@ class SaltContactsServiceProvider extends ServiceProvider
          * Uncomment the second function call to load the JSON translations.
          * Uncomment the third function call to make the translations publishable using the 'translations' tag.
          */
-        // $this->loadTranslationsFrom(__DIR__.'/../../resources/lang', 'salt-laravel-contacts');
-        // $this->loadJsonTranslationsFrom(__DIR__.'/../../resources/lang', 'salt-laravel-contacts');
+        // $this->loadTranslationsFrom(__DIR__.'/../../resources/lang', 'salt-contacts');
+        // $this->loadJsonTranslationsFrom(__DIR__.'/../../resources/lang', 'salt-contacts');
         // $this->publishes([
-        //     __DIR__.'/../../resources/lang' => resource_path('lang/vendor/salt-laravel-contacts'),
+        //     __DIR__.'/../../resources/lang' => resource_path('lang/vendor/salt-contacts'),
         // ], 'translations');
 
         /**
@@ -49,9 +50,9 @@ class SaltContactsServiceProvider extends ServiceProvider
          * Uncomment the first section to load the views.
          * Uncomment the second section to make the view publishable using the 'view' tags.
          */
-        // $this->loadViewsFrom(__DIR__.'/../../resources/views', 'salt-laravel-contacts');
+        // $this->loadViewsFrom(__DIR__.'/../../resources/views', 'salt-contacts');
         // $this->publishes([
-        //     __DIR__.'/../../resources/views' => resource_path('views/vendor/salt-laravel-contacts'),
+        //     __DIR__.'/../../resources/views' => resource_path('views/vendor/salt-contacts'),
         // ], 'views');
 
         /**
@@ -72,7 +73,7 @@ class SaltContactsServiceProvider extends ServiceProvider
          * Uncomment this functin call to make the public assets publishable using the 'public' tag.
          */
         // $this->publishes([
-        //     __DIR__.'/../../public' => public_path('vendor/salt-laravel-contacts'),
+        //     __DIR__.'/../../public' => public_path('vendor/salt-contacts'),
         // ], 'public');
 
         /**
@@ -81,10 +82,10 @@ class SaltContactsServiceProvider extends ServiceProvider
          * Uncomment the first function call to load the migrations.
          * Uncomment the second function call to make the migrations publishable using the 'migrations' tags.
          */
-        // $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
-        // $this->publishes([
-        //     __DIR__.'/../../database/migrations/' => database_path('migrations')
-        // ], 'migrations');
+        $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
+        $this->publishes([
+            __DIR__.'/../../database/migrations/' => database_path('migrations')
+        ], 'contacts-migrations');
     }
 
     /**
@@ -101,7 +102,7 @@ class SaltContactsServiceProvider extends ServiceProvider
          * If the config file is also publishable, it will merge with that file
          */
         // $this->mergeConfigFrom(
-        //     __DIR__.'/../../config/salt-laravel-contacts.php', 'salt-laravel-contacts'
+        //     __DIR__.'/../../config/salt-contacts.php', 'salt-contacts'
         // );
     }
 }
