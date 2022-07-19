98cce5f04a40c73ad447c6f41ff02bc4cb49fad2
Farid Hidayat <farid.hidayat@sagara.id>
2022-07-19 11:43:32 +0700

    refactor: uuid comply trait

--- src/Models/ContactSocials.php
+++ src/Models/ContactSocials.php
@@ -10,10 +10,13 @@ use Illuminate\Support\Facades\Schema;
 
 use SaltLaravel\Models\Resources;
 use SaltLaravel\Traits\ObservableModel;
+use SaltLaravel\Traits\Uuids;
 
 class ContactSocials extends Resources {
 
+    use Uuids;
     use ObservableModel;
+
     protected $filters = [
         'default',
         'search',
